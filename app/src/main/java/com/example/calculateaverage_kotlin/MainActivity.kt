9c0a92b8066707bc1e8be41e40c8831cc9684685
fahricandurucan <fahricandurucan@icloud.com>
2023-03-01 10:00:15 +0300

    added delete method and made some fixes

--- app/src/main/java/com/example/calculateaverage_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculateaverage_kotlin/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.new_course_layout.*
 import kotlinx.android.synthetic.main.new_course_layout.view.*
@@ -15,30 +18,61 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if(rootLayout.childCount==0){
+            btnCalculate.visibility = View.INVISIBLE
+        }
+        else{
+            btnCalculate.visibility = View.VISIBLE
+        }
         btnAddCourse.setOnClickListener {
-            var inflater = LayoutInflater.from(this)
-            //var inflater2 = layoutInflater
-            /*var inflater3 = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            inflater3.inflate()*/
 
-            //XML den javaya dönüştürme
-            // yani new_course_layout u alıp view a dönüştürüyoruz
-            var newCourseView = inflater.inflate(R.layout.new_course_layout,null)
+            if(!etCourseName.text.isNullOrEmpty()){
+                var inflater = LayoutInflater.from(this)
+                //var inflater2 = layoutInflater
+                /*var inflater3 = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                inflater3.inflate()*/
 
-            var courseName = etCourseName.text.toString()
-            var kredi = spinnerKredi.selectedItem.toString()
-            var note = spinnerNote.selectedItem.toString()
+                //XML den javaya dönüştürme
+                // yani new_course_layout u alıp view a dönüştürüyoruz
+                var newCourseView = inflater.inflate(R.layout.new_course_layout,null)
 
-            newCourseView.etNewCourseName.setText(courseName)
-            newCourseView.spinnerNewKredi.setSelection(getSpinnerPosition(spinnerKredi,kredi))
-            newCourseView.spinnerNewNote.setSelection(getSpinnerPosition(spinnerNote,note))
+                var courseName = etCourseName.text.toString()
+                var kredi = spinnerKredi.selectedItem.toString()
+                var note = spinnerNote.selectedItem.toString()
 
-            rootLayout.addView(newCourseView)
+                newCourseView.etNewCourseName.setText(courseName)
+                newCourseView.spinnerNewKredi.setSelection(getSpinnerPosition(spinnerKredi,kredi))
+                newCourseView.spinnerNewNote.setSelection(getSpinnerPosition(spinnerNote,note))
+
+
+                //silme butonu oluşturuluyor
+                newCourseView.buttonDelete.setOnClickListener {
+                    rootLayout.removeView(newCourseView)
+                    if(rootLayout.childCount==0){
+                        btnCalculate.visibility = View.INVISIBLE
+                    }
+                    else{
+                        btnCalculate.visibility = View.VISIBLE
+                    }
+                }
+                rootLayout.addView(newCourseView)
+                btnCalculate.visibility = View.VISIBLE
+                reset()
+            }
+            else{
+                Toast.makeText(this,"Lütfen Ders Adını Giriniz",Toast.LENGTH_LONG).show()
+            }
+
 
         }
 
 
+    }
 
+    fun reset(){
+        etCourseName.setText("")
+        spinnerKredi.setSelection(0)
+        spinnerNote.setSelection(0)
     }
 
     fun getSpinnerPosition(spinner: Spinner, value:String) : Int {
