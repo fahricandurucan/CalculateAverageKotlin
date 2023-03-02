0d5a9c8b40b518a7fd75a8a2b525ae5bddc712db
fahricandurucan <fahricandurucan@icloud.com>
2023-03-02 11:04:42 +0300

    added method of calculate average

---
+++ app/src/main/java/com/example/calculateaverage_kotlin/Courses.kt
@@ -0,0 +1,5 @@
+package com.example.calculateaverage_kotlin
+
+class Courses (var name:String, var note:String, var kredi:String){
+
+}
--- app/src/main/java/com/example/calculateaverage_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculateaverage_kotlin/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.calculateaverage_kotlin
 
 import android.content.Context
+import android.icu.number.IntegerWidth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,6 +9,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.view.get
 import androidx.core.view.isEmpty
 import androidx.core.view.isNotEmpty
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private val COURSES = arrayOf("Calculus","General Physics","Data Structure","Matematik","Kimya",
     "Algorithm","Operating System")
+    private var coursesArray = ArrayList<Courses>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +69,9 @@ class MainActivity : AppCompatActivity() {
                 rootLayout.addView(newCourseView)
                 btnCalculate.visibility = View.VISIBLE
                 reset()
+
+
+
             }
             else{
                 Toast.makeText(this,"Lütfen Ders Adını Giriniz",Toast.LENGTH_LONG).show()
@@ -98,6 +104,44 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateAverage(view: View){    // view a hangi butona tıkladıysam onun bilgisi geliyor diyebilirim
 
+        var sumOfKredi=0.0
+        var sumOfCourse=0.0
+
+        for(i in 0..rootLayout.childCount-1){
+            var line = rootLayout.getChildAt(i)
+            var course = Courses(line.etNewCourseName.text.toString(),
+                (line.spinnerNewNote.selectedItem.toString()),
+                (line.spinnerNewKredi.selectedItemPosition+1).toString())
+            coursesArray.add(course)
+        }
+
+
+        for(currentCourse in coursesArray){
+            sumOfKredi += currentCourse.kredi.toDouble()
+            sumOfCourse += currentCourse.kredi.toDouble()*convertNoteToKredi(currentCourse.note)
+        }
+
+        var result = sumOfCourse / sumOfKredi
+        Toast.makeText(this,"ORTALAMA: "+result,Toast.LENGTH_LONG).show()
+        coursesArray.clear() //butona tekrar tekrar basıldığında hesaplama yapmasın diye yazıldı
+
+
+
+    }
+
+    fun convertNoteToKredi(note:String):Double{
+        var value = 0.0
+        when(note){
+            "AA" -> value = 4.0
+            "BA" -> value = 3.5
+            "BB" -> value = 3.0
+            "CB" -> value = 2.5
+            "CC" -> value = 2.0
+            "DC" -> value = 1.5
+            "DD" -> value = 1.0
+            "FF" -> value = 0.0
+        }
+        return  value
     }
 
 }
