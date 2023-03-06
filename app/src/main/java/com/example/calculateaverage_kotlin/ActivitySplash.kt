a6a5cf7dbbbc18521b3428f04e804cfd51297c33
fahricandurucan <fahricandurucan@icloud.com>
2023-03-06 16:25:58 +0300

    page transition created

--- app/src/main/java/com/example/calculateaverage_kotlin/ActivitySplash.kt
+++ app/src/main/java/com/example/calculateaverage_kotlin/ActivitySplash.kt
@@ -1,11 +1,19 @@
 package com.example.calculateaverage_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class ActivitySplash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        buttonMain.setOnClickListener{
+            var intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
